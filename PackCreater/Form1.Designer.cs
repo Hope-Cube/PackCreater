e38150ba9a8a893982d38e1c69ecf67ef58801f0
Hope-Cube <hopecubehh@gmail.com>
2025-11-20 10:39:08 +0100

    2025.11.20.1

--- PackCreater/Form1.Designer.cs
+++ PackCreater/Form1.Designer.cs
@@ -38,8 +38,11 @@
             description_check = new CheckBox();
             description = new TextBox();
             fBD = new FolderBrowserDialog();
-            pack_create = new Button();
             folder_select = new Button();
+            selected_folder = new Label();
+            folder_name_label = new Label();
+            folder_name = new TextBox();
+            create = new Button();
             SuspendLayout();
             // 
             // pack_type_label
@@ -54,6 +57,7 @@
             // 
             // pack_type
             // 
+            pack_type.BackColor = SystemColors.Window;
             pack_type.DropDownStyle = ComboBoxStyle.DropDownList;
             pack_type.FormattingEnabled = true;
             pack_type.Items.AddRange(new object[] { "Data pack", "Resource pack" });
@@ -83,7 +87,7 @@
             // 
             version_label.AutoSize = true;
             version_label.Font = new Font("Consolas", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            version_label.Location = new Point(12, 79);
+            version_label.Location = new Point(12, 100);
             version_label.Name = "version_label";
             version_label.Size = new Size(56, 14);
             version_label.TabIndex = 7;
@@ -93,11 +97,12 @@
             // 
             version.DropDownStyle = ComboBoxStyle.DropDownList;
             version.FormattingEnabled = true;
-            version.Items.AddRange(new object[] { "1.13 - 1.14.4", "1.15 - 1.16.2", "1.16.3 - 1.16.5", "1.17 - 1.17.1", "1.18 - 1.18.1", "1.18.2", "1.19 - 1.19.3", "1.19.4", "1.20 - 1.20.1", "1.20.2", "1.20.3 - 1.20.4", "1.20.5 - 1.20.6", "1.21 - 1.21.1", "1.21.2 - 1.21.3", "1.21.5", "1.21.6", "1.21.7 - 1.21.8", "1.21.9 - 1.21.10" });
-            version.Location = new Point(12, 96);
+            version.ItemHeight = 14;
+            version.Items.AddRange(new object[] { "1.13 - 1.14.4", "1.15 - 1.16.1", "1.16.2 - 1.16.5", "1.17 - 1.17.1", "1.18 - 1.18.1", "1.18.2", "1.19 - 1.19.3", "1.19.4", "1.20 - 1.20.1", "1.20.2", "1.20.3 - 1.20.4", "1.20.5 - 1.20.6", "1.21 - 1.21.1", "1.21.2 - 1.21.3", "1.21.5", "1.21.6", "1.21.7 - 1.21.8", "1.21.9 - 1.21.10" });
+            version.Location = new Point(12, 117);
             version.Name = "version";
-            version.Size = new Size(121, 22);
-            version.TabIndex = 8;
+            version.Size = new Size(138, 22);
+            version.TabIndex = 4;
             // 
             // description_label
             // 
@@ -115,7 +120,7 @@
             description_check.Location = new Point(12, 166);
             description_check.Name = "description_check";
             description_check.Size = new Size(15, 14);
-            description_check.TabIndex = 10;
+            description_check.TabIndex = 5;
             description_check.UseVisualStyleBackColor = true;
             description_check.Click += description_check_Click;
             // 
@@ -126,35 +131,66 @@
             description.Multiline = true;
             description.Name = "description";
             description.Size = new Size(262, 66);
-            description.TabIndex = 11;
+            description.TabIndex = 6;
             description.Visible = false;
-            // 
-            // pack_create
-            // 
-            pack_create.Location = new Point(135, 351);
-            pack_create.Name = "pack_create";
-            pack_create.Size = new Size(75, 23);
-            pack_create.TabIndex = 12;
-            pack_create.Text = "Create";
-            pack_create.UseVisualStyleBackColor = true;
             // 
             // folder_select
             // 
-            folder_select.Location = new Point(93, 275);
+            folder_select.Location = new Point(12, 279);
             folder_select.Name = "folder_select";
-            folder_select.Size = new Size(104, 50);
-            folder_select.TabIndex = 13;
-            folder_select.Text = "Select root folder";
+            folder_select.Size = new Size(81, 23);
+            folder_select.TabIndex = 7;
+            folder_select.Text = "Select";
             folder_select.UseVisualStyleBackColor = true;
             folder_select.Click += folder_select_Click;
+            // 
+            // selected_folder
+            // 
+            selected_folder.AutoSize = true;
+            selected_folder.Location = new Point(12, 262);
+            selected_folder.Name = "selected_folder";
+            selected_folder.Size = new Size(119, 14);
+            selected_folder.TabIndex = 14;
+            selected_folder.Text = "Select folder...";
+            // 
+            // folder_name_label
+            // 
+            folder_name_label.AutoSize = true;
+            folder_name_label.Font = new Font("Consolas", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            folder_name_label.Location = new Point(146, 51);
+            folder_name_label.Name = "folder_name_label";
+            folder_name_label.Size = new Size(84, 14);
+            folder_name_label.TabIndex = 16;
+            folder_name_label.Text = "Folder name";
+            // 
+            // folder_name
+            // 
+            folder_name.Location = new Point(146, 68);
+            folder_name.Name = "folder_name";
+            folder_name.Size = new Size(128, 22);
+            folder_name.TabIndex = 3;
+            // 
+            // create
+            // 
+            create.Location = new Point(12, 308);
+            create.Name = "create";
+            create.Size = new Size(262, 23);
+            create.TabIndex = 8;
+            create.Text = "Create";
+            create.UseVisualStyleBackColor = true;
+            create.Click += create_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 14F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(286, 420);
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(286, 340);
+            Controls.Add(create);
+            Controls.Add(folder_name_label);
+            Controls.Add(folder_name);
+            Controls.Add(selected_folder);
             Controls.Add(folder_select);
-            Controls.Add(pack_create);
             Controls.Add(description);
             Controls.Add(description_check);
             Controls.Add(description_label);
@@ -165,7 +201,11 @@
             Controls.Add(pack_type);
             Controls.Add(pack_type_label);
             Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Pixel);
+            MaximumSize = new Size(302, 379);
+            MinimumSize = new Size(302, 379);
             Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "PackCreater";
             ResumeLayout(false);
             PerformLayout();
@@ -183,7 +223,10 @@
         private CheckBox description_check;
         private TextBox description;
         private FolderBrowserDialog fBD;
-        private Button pack_create;
         private Button folder_select;
+        private Label selected_folder;
+        private Label folder_name_label;
+        private TextBox folder_name;
+        private Button create;
     }
 }
--- PackCreater/Form1.cs
+++ PackCreater/Form1.cs
@@ -5,6 +5,12 @@ namespace PackCreater
         public Form1()
         {
             InitializeComponent();
+
+            description_check.Checked = false;
+            description.Visible = false;
+            description.Enabled = false;
+            selected_folder.Location = new(12, 212);
+            folder_select.Location = new(12, 229);
         }
 
         private void description_check_Click(object sender, EventArgs e)
@@ -14,20 +20,95 @@ namespace PackCreater
                 description.Visible = true;
                 description.Enabled = true;
                 description.Focus();
+                selected_folder.Location = new(12, 262);
+                folder_select.Location = new(12, 279);
             }
             else
             {
                 description.Visible = false;
                 description.Enabled = false;
+                selected_folder.Location = new(12, 212);
+                folder_select.Location = new(12, 229);
             }
         }
 
         private void folder_select_Click(object sender, EventArgs e)
         {
-            if(fBD.ShowDialog() == DialogResult.OK)
+            if (fBD.ShowDialog() == DialogResult.OK)
             {
-                pack_name.Text = fBD.SelectedPath;
+                DirectoryInfo dir = new(fBD.SelectedPath);
+
+                // last folder and its parent
+                string lastTwo = dir.Parent != null
+                    ? $@"{dir.Parent.Name}\{dir.Name}"
+                    : dir.Name;
+
+                selected_folder.Text = @"...\" + lastTwo;
             }
+        }
+
+        private Dictionary<byte, byte> datapackFormats = new()
+        {
+            { 0, 4 },
+            { 1, 5 },
+            { 2, 6 },
+            { 3, 7 },
+            { 4, 8 },
+            { 5, 9 },
+            { 6, 10 },
+            { 7, 12 },
+            { 8, 15 },
+            { 9, 18 },
+            { 10, 26 },
+            { 11, 41 },
+            { 12, 48 },
+            { 13, 57 },
+            { 14, 61 },
+            { 15, 71 },
+            { 16, 80 },
+            { 17, 81 },
+            { 18, 88 }
+        };
+        private void create_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(Path.Combine(pack_name.Text, "data", folder_name.Text, "function"));
+            File.Create(Path.Combine(pack_name.Text, "data", folder_name.Text, "function", "test.mcfunction")).Close();
+            using StreamWriter writer = new(Path.Combine(pack_name.Text, "pack.mcmeta"));
+            writer.WriteLine("{");
+            writer.WriteLine("  \"pack\": {");
+            writer.WriteLine($"    \"pack_format\": {datapackFormats[(byte)(version.SelectedIndex + 1)]},");
+            writer.WriteLine($"    \"description\": \"{(description_check.Checked ? description.Text : "A Minecraft pack created using PackCreater")}\"");
+            writer.WriteLine("  }");
+            writer.WriteLine("}");
+
+            using Bitmap bitmap = new(128, 128);
+            using Graphics g = Graphics.FromImage(bitmap);
+            Random rnd = new();
+            g.Clear(Color.FromArgb(255, rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)));
+            string text = "PC";
+
+            SizeF size;
+            float x, y;
+            // measure the text
+            using (Font font = new("Consolas", 58, FontStyle.Bold))
+            { 
+                size = g.MeasureString(text, font);
+                x = (128 - size.Width) / 2f;
+                y = (128 - size.Height) / 2f;
+                g.DrawString(text, font, Brushes.Black, new PointF(x, y));
+            }
+
+            using (Font font = new("Consolas", 52))
+            {
+                size = g.MeasureString(text, font);
+                x = (128 - size.Width) / 2f;
+                y = (128 - size.Height) / 2f;
+                g.DrawString(text, font, Brushes.White, new PointF(x, y));
+            }
+
+            bitmap.Save(Path.Combine(pack_name.Text, "pack.png"));
+
+            MessageBox.Show("Pack created successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
